88d9b57d3e5295a8293211d054bd1bf8e310ee7f
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-09-15 22:12:05 +0900

    [level 3] Title: 징검다리 건너기, Time: 3.68 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -1,17 +1,16 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
-bool binary_search(vector<int> &stones, int k, int mid)
-{
-    int cnt =0;
-    for(int i=0;i<stones.size();i++)
-    {
-        if(stones[i] < mid )    cnt++;
-        else    cnt=0;
-        
-        if(cnt >= k)    return false;
+
+bool binary_check(vector<int>& stones, int k, int mid){
+    int cnt = 0;
+    for(int i = 0; i < stones.size(); i++){
+        if(stones[i] <= mid) cnt += 1;
+        else cnt = 0;
+        if(cnt >= k) return false;
     }
     return true;
 }
@@ -19,17 +18,18 @@ bool binary_search(vector<int> &stones, int k, int mid)
 int solution(vector<int> stones, int k) {
     int answer = 0;
     int left = 1;
-    int right = *max_element(stones.begin(),stones.end());
+    int right = *max_element(stones.begin(), stones.end());
     
     while(left <= right){
-        int mid = (left+right)/2;
-        if(binary_search(stones,k,mid)){
-            left = mid +1;
-            if(answer < mid)    answer = mid;
+        int mid = (left + right)/2;
+        if(!binary_check(stones, k, mid)){
+            right = mid-1;
+            answer = mid;
         }
         else{
-            right = mid -1;
-        }     
+            left = mid+1;
+            
+        }
     }
     return answer;
 }
