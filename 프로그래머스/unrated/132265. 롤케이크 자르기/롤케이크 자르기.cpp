cacaa7f5128693541d0ade65c7a1dbfa833b126a
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-09-23 23:08:05 +0900

    [unrated] Title: 롤케이크 자르기, Time: 3.89 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+const int MAXIMUM = 10001;
+int solution(vector<int> topping) {
+    int answer = 0;
+    int* arr = new int[MAXIMUM];
+    bool* visited = new bool[MAXIMUM];
+    int topping_size = 0;
+    fill_n(arr, MAXIMUM, -1);
+    fill_n(visited, MAXIMUM, false);
+    for(int num : topping){
+        if(arr[num] < 0){
+            topping_size++;
+            arr[num] = 0;
+        }
+        arr[num]++;
+    }
+    cout << topping_size << endl;
+    int left = 0;
+    int right = topping_size;
+    for(int num : topping){
+        if(!visited[num]){
+            visited[num] = true;
+            left++;
+        }
+        
+        arr[num]--;
+        if(arr[num] == 0){
+            right--;
+        }
+        
+        if(left == right){
+            answer++;
+        }
+        
+        if(left > right)
+            break;
+    }
+    return answer;
+}
