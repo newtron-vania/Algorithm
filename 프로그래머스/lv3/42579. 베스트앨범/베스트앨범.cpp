122a48a3091ffb45fbbc4ac0d943a1b5b072928e
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-09-23 21:43:22 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.cpp
@@ -0,0 +1,54 @@
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool compare1(vector<int> a, vector<int> b) {
+    if (a[0] > b[0]) {
+        return true;
+    }
+    else if (a[0] == b[0]) {
+        return a[1] < b[1];
+    }
+    return false;
+}
+
+bool compare2(const pair<string, int>& a, const pair<string, int>& b) {
+    return a.second > b.second;
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    unordered_map<string, vector<vector<int>>> sortDB;
+    map<string, int> scoreDB;
+
+    for (int i = 0; i < genres.size(); i++) {
+        sortDB[genres[i]].push_back({ plays[i], i });
+        scoreDB[genres[i]] += plays[i];
+    }
+
+    for (auto& itr : sortDB) {
+        sort(itr.second.begin(), itr.second.end(), compare1);
+    }
+    vector<pair<string, int> > vec;
+
+    for (auto& itr : scoreDB) {
+        vec.emplace_back(itr);
+    }
+    sort(vec.begin(), vec.end(), compare2);
+
+    for (auto& itr : vec) {
+        int size = sortDB[itr.first].size() >= 2 ? 2 : sortDB[itr.first].size();
+        for (int i = 0; i < size; i++) {
+            answer.push_back(sortDB[itr.first][i][1]);
+        }
+    }
+
+
+    return answer;
+}
