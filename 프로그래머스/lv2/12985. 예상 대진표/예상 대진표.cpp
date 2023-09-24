f968e2aa0ae847f392712bf5f647c68486917c78
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-09-24 22:26:52 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -5,17 +5,10 @@ using namespace std;
 int solution(int n, int a, int b)
 {
     int answer = 0;
-    int size = 0;
-    while(n>0){
-        size += 1;
-        n/=2;
-    }
-    
-    for(int i = 1; i <size; i++){
+    while(a!=b){
         a = (a+1)/2;
         b = (b+1)/2;
         answer += 1;
-        if(a == b ) return answer;
     }
     return answer;
 }
