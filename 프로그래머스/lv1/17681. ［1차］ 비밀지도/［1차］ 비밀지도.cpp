744e20d68be34327b53dec550283789ba381ca4e
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-09-24 22:11:00 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    int size = arr1.size();
+    for(int i = 0; i < size; i++){
+        int sum = arr1[i] | arr2[i];
+        string str = "";
+        for(int j = 0; j < size; j++){
+            if(sum%2) str = "#" + str;
+            else str = " " + str;
+            sum /= 2;
+        }
+        answer.push_back(str);
+    }
+    return answer;
+}
