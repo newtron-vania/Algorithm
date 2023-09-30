12304ff406f4cf634737123086806bf5bc157046
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-09-30 22:48:33 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+const int MAX = 501;
+
+bool comp(pair<int, int>& a, pair<int, int>& b){
+    return a.second > b.second;
+}
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    unordered_map<int, int> n_cnts;
+    
+    int idx = 0;
+    while(idx < s.length()){
+        string tmp = "";
+        
+        while(isdigit(s[idx])){
+            tmp += s[idx];
+            idx++;
+        }
+        
+        idx++;
+        if(tmp.empty())
+            continue;
+        
+        n_cnts[stoi(tmp)] += 1;
+        
+    }
+    vector<pair<int, int>> n_vec(n_cnts.begin(), n_cnts.end());
+    
+    sort(n_vec.begin(), n_vec.end(), comp);
+    for(auto& itr : n_vec){
+        answer.push_back(itr.first);
+    }
+    
+    return answer;
+}
