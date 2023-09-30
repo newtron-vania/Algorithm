892b831912b3485852e9f8a7d4dc343c6af600c3
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-09-30 21:23:53 +0900

    [unrated] Title: 덧칠하기, Time: 0.17 ms, Memory: 4.98 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
@@ -5,17 +5,13 @@ using namespace std;
 
 int solution(int n, int m, vector<int> section) {
     int answer = 1;
-    if(section.size() <= 1)
-        return answer;
-    
-    
     int start_section = section[0];
-    for(int i = 1; i < section.size(); i++){
-        if(section[i] - start_section < m){
+    for(auto& sector : section){
+        if(sector - start_section < m){
             continue;
         }
         else{
-            start_section = section[i];
+            start_section = sector;
             answer++;
         }
     }
