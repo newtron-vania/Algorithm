ac2c6d7a5b2c09b0a365aeb83219c126ecf153ae
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-11-06 17:49:35 +0900

    [level 3] Title: 스티커 모으기(2), Time: 0.77 ms, Memory: 6.99 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -16,11 +16,13 @@ int solution(vector<int> sticker)
     dp[0] = pair(sticker[0], 0);
     dp[1] = pair(sticker[0], sticker[1]);
     //점화식 : dp[n] = max(dp[n-1], dp[n] + dp[n-2])
-    bool first_last = false;
+    
+    //처음을 찢었다면 마지막 스티커까지의 최댓값을 구할 필요가 없고,
+    //처음을 찢지 않았다면 마지막 스티커까지의 최댓값을 구해야 한다.
+    //즉, 마지막 스티커 이전까지의 최댓값을 각각 구하고, 마지막 스티꺼를 포함하여 처음을 찢지 않았을 때의 최댓값을 추가로 구한 후 두 경우 중 최댓값을 구한다.
     for(int i = 2; i < size-1; i++)
     {
         int first = max(dp[i-1].first, sticker[i] + dp[i-2].first);
-        first_last = dp[i-1].first < sticker[i] + dp[i-2].first;
         int second = max(dp[i-1].second, sticker[i] + dp[i-2].second);
         dp[i] = pair(first, second);
     }
