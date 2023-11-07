1d19c6f819ece3596e5e6b775e3e39ddfe937b0c
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-11-07 17:34:51 +0900

    [level 2] Title: [1차] 캐시, Time: 0.26 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+//LRU : 가장 최근에 사용한 순서대로 우선순위가 높은 알고리즘
+//데이터의 삽입과 삭제가 자유로워야 한다 : LinkedList
+
+//Node
+/**
+데이터와 앞뒤 노드 데이터를 저장.
+해당 노드가 제거될 시 앞 노드와 뒤 노드를 연결한다.
+**/
 class Node {
 public:
     string city;
@@ -17,7 +25,7 @@ public:
     Node* head = NULL;
     Node* tail = NULL;
     int size = 0;
-
+    //노드 추가
     void AddNodeToFront(const string& _city) {
         size += 1;
         Node* newNode = new Node;
@@ -32,7 +40,10 @@ public:
             head = newNode;
         }
     }
-
+    //노드 탐색
+    //노드가 없다면 새로운 노드를 추가
+    //노드가 없는데 캐시가 꽉 찾다면 우선순위가 가장 낮은 노드(tail)을 제거 후 새로운 노드를 추가
+    //노드가 있다면 해당 노드를 제거하고 노드를 추가
     Node* SearchNode(const string& city) {
         Node* tmp = head;
         while (tmp != NULL) {
@@ -44,6 +55,8 @@ public:
         return NULL;
     }
 
+    //노드 제거
+    //노드 제거 후 앞 뒤 노드를 연결한다.
     void RemoveNode(Node* searchedNode) {
         size -= 1;
         if (head == searchedNode) {
