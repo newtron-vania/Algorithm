b346f66eb5c3bc5f0851ec82d640d9fae692cf89
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-11-07 17:13:01 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -7,11 +7,10 @@ using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
     map<string, int> wordDict;
-    string lastWord = words[0];
     wordDict.insert(make_pair(words[0], 1));
     int cnt = 1;
     for(int i = 1; i < words.size(); i++){
-        if(lastWord.back() != words[i].front()){
+        if(words[i-1].back() != words[i].front()){
             break;
         }
         else if(wordDict.find(words[i]) != wordDict.end()){
@@ -19,16 +18,14 @@ vector<int> solution(int n, vector<string> words) {
         }
         else{
             wordDict.insert(make_pair(words[i], 1));
-            lastWord = words[i];
         }
         cnt++;
     }
-    cout << cnt;
     if(cnt < words.size()){
-        return {cnt%n+1, cnt/n +1};
+        return {cnt%n + 1, cnt/n + 1};
     }
     else{
         return {0, 0};
     }
-
+    
 }
