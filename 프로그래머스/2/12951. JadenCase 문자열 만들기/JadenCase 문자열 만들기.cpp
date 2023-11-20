01c52222af47ca3e00dd0688f61bc8aaf7cc3ffb
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-11-20 17:48:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -1,35 +1,15 @@
 #include <string>
 #include <vector>
-#include <sstream>
 
 using namespace std;
 
 string solution(string s) {
     string answer = "";
     
-    bool start = true;
-    for(int i = 0; i < s.length(); i++)
+    answer += toupper(s[0]);
+    for(int i = 1; i < s.length(); i++)
     {
-        if(isspace(s[i]))
-        {
-            start = true;
-        }
-        else
-        {
-            if(!isdigit(s[i]))
-            {
-                if(start)
-                {
-                    s[i] = toupper(s[i]);
-                }
-                else
-                {
-                    s[i] = tolower(s[i]);
-                }
-            }
-            start = false;
-        }
-        answer += s[i];
+        answer += isspace(s[i-1]) ? toupper(s[i]) : tolower(s[i]);
     }
     
     return answer;
