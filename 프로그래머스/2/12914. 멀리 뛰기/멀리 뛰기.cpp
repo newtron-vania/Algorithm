7b833d774f2af8285b442f5c8620dd7fabc8925f
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-11-21 20:14:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int FIB_VALUE = 1234567;
+
+long long solution(int n) {
+    if(n == 1)
+        return 1;
+    else if(n==2)
+        return 2;
+    int answer = 0;
+    int *dp = new int[n+1];
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3; i <= n; i++)
+    {
+        dp[i] = (dp[i-1] + dp[i-2]) % FIB_VALUE;
+    }
+    
+    answer = dp[n];
+    delete[] dp;
+    return answer;
+}
