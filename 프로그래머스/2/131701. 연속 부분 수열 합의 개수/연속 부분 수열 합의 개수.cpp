619c8add8130404eb498b4ae5761f1b458c699fd
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-11-22 17:57:18 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 326.75 ms, Memory: 23.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+set<int> nums;
+
+int solution(vector<int> elements) {
+    int answer = 0;
+    vector<int> circle_elements = elements;
+    circle_elements.insert(circle_elements.end(), elements.begin(), elements.end());
+    
+    for(int i = 0; i < circle_elements.size() / 2; i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < circle_elements.size() / 2; j++)
+        {
+            sum += circle_elements[i + j];
+            nums.insert(sum);
+        }
+    }
+    
+    answer = nums.size();
+    return answer;
+}
