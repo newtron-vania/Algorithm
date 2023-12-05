db73b0340b9a90777a3eafb61729f73e91b95b80
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-12-05 18:42:18 +0900

    [level 3] Title: 가장 먼 노드, Time: 14.71 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -15,17 +15,17 @@ public:
 };
 
 int solution(int n, vector<vector<int>> edge) {
-    int answer = 0;
+    int answer = -1;
 
     vector<Node*> graph(n + 1, nullptr);
     vector<bool> visited(n + 1, false);
 
-    // 새 노드들을 생성
+    //새 노드들을 생성
     for (int i = 1; i <= n; ++i) {
         graph[i] = new Node(i);
     }
 
-    // edge를 통해 노드간 연결을 확인
+     //edge를 통해 노드간 연결을 확인
     for (vector<int> e : edge) {
         int from = e[0];
         int to = e[1];
@@ -33,32 +33,28 @@ int solution(int n, vector<vector<int>> edge) {
         graph[to]->neighbors.push_back(graph[from]);
     }
 
-    queue<vector<Node*>> q;
-    q.push({graph[1]});
+    queue<Node*> q;
+    q.push(graph[1]);
     visited[1] = true;
-
+    
     //같은 길이의 노드들의 다음 노드들을 참조하고 방문했던 노드가 아니라면 다음 해당 노드를 저장하고, 같은 길이의 노드 개수를 answer에 저장한다.
     while (!q.empty()) {
-        vector<Node*> from = q.front();
-        q.pop();
-        vector<Node*> to;
-        for (Node* curr : from) {
+        int levelSize = q.size();
+        for (int i = 0; i < levelSize; ++i) {
+            Node* curr = q.front();
+            q.pop();
+
             for (Node* neighbor : curr->neighbors) {
                 if (!visited[neighbor->val]) {
                     visited[neighbor->val] = true;
-                    to.push_back(neighbor);
+                    q.push(neighbor);
                 }
             }
         }
-        if(to.size() > 0)
-        {
-            q.push(to);
-            answer = to.size();
-        }
-        
+        answer = levelSize;
     }
 
-    // 메모리 해제
+    //메모리 해제
     for (int i = 1; i <= n; ++i) {
         delete graph[i];
     }
