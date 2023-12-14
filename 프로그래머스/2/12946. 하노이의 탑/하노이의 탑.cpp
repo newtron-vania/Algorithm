83eb5501f44c403ebea474a6fb94ab46d0ea1f49
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-12-14 23:40:34 +0900

    [level 2] Title: 하노이의 탑, Time: 27.81 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void hanoi(int n, int go, int to, vector<vector<int>>& answer) {
+    if (n == 1) {// 쌓인 원판이 한개라면 
+        answer.push_back({go, to});
+        return ;
+    }
+    hanoi(n-1, go, 6-go-to, answer);// n-1 개의 원판을 시작점, 목표점을 제외한 곳으로 이동
+    hanoi(1, go, to, answer);// n번째 원판을 목표지점으로 이동
+    hanoi(n-1, 6-go-to, to, answer);// n-1 개의 원판을 목표점으로 이동
+}
+
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer;
+    hanoi(n, 1, 3, answer);
+    return answer;
+}
