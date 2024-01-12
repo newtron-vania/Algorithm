7eb3c254ab53ad257ccd442349aed96c41667d4a
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-01-12 23:27:57 +0900

    [level Lv.4] Title: 징검다리, Time: 3.83 ms, Memory: 4.67 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.4/43236. 징검다리/징검다리.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+    int answer = 0;
+    int start = 1;
+    int end = distance;
+    int mid;
+    int cnt, prev;
+    
+    sort(rocks.begin(), rocks.end());
+    
+    while(start <= end){
+        mid = (start+end)/2;
+        cnt = 0;
+        prev = 0;
+        
+        for (int i = 0; i < rocks.size(); ++i){
+            if(mid > rocks[i] - prev)
+                ++cnt;
+            else
+                prev = rocks[i];
+        }
+        
+        if(mid > distance - prev)
+        {
+            ++cnt;
+        }
+        
+        
+        if(cnt > n)
+        {
+            end = mid-1;
+        }
+        else
+        {
+            start = mid+1;
+            answer = mid;
+        }
+    }
+    return answer;
+}
