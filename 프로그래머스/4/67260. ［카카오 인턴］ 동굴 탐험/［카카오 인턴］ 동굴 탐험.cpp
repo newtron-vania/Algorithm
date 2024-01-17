0841437ff1b6dd732b5d00533a27f280b921e74f
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-01-17 18:30:19 +0900

    [level 4] Title: [카카오 인턴] 동굴 탐험, Time: 72.20 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/4/67260. ［카카오 인턴］ 동굴 탐험/［카카오 인턴］ 동굴 탐험.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+using namespace std;
+const int MAX = 200000; // 2e5
+
+vector<int> edge[MAX];
+set<int> s;
+int key[MAX], needKey[MAX];
+bool check_visit[MAX];
+
+void BFS(int node){
+    queue<int> q;
+    q.push(node);
+    check_visit[node] = true;
+    
+    if(needKey[0] != 0) return; 
+    
+    while(!q.empty())
+    {
+        int tmp = q.front();
+        q.pop();
+        needKey[key[tmp]] = 0;
+        
+        if(s.find(key[tmp]) != s.end())
+        { 
+            s.erase(key[tmp]); 
+            q.push(key[tmp]); 
+            check_visit[key[tmp]] = 1;
+        }
+        for(int it : edge[tmp])
+        {
+            if(check_visit[it]) continue; 
+            if(needKey[it] != 0)
+            { 
+                s.insert(it);
+                continue;
+            }
+            check_visit[it] = true;
+            q.push(it);
+        }
+    }
+}
+
+bool solution(int n, vector<vector<int>> path, vector<vector<int>> order) {
+    for(auto &it : path)
+    {
+        edge[it[0]].push_back(it[1]);
+        edge[it[1]].push_back(it[0]);
+    }
+
+    for(auto & it : order)
+    {
+        key[it[0]] = it[1];
+        needKey[it[1]] = it[0]; 
+    }
+    
+    BFS(0);
+    for(int i = 0 ; i < n ; i++) if(!check_visit[i]) return false;
+    return true;
+}
