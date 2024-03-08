517a723e78163a3530ce4ce315744cb01ee05f54
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-03-08 22:15:43 +0900

    [level 4] Title: 쿠키 구입, Time: 3.29 ms, Memory: 3.79 MB -BaekjoonHub

--- 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.cpp
+++ 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// 투 포인터 누적합
 int solution(vector<int> cookies) {
     int answer = 0;
 
@@ -10,23 +11,29 @@ int solution(vector<int> cookies) {
     {
         int left = i;
         int right = i+1;
+        // left ~ i 의 누적합
         int ls = cookies[left];
+        // i ~ right 누적합
         int rs = cookies[right];
         
         while(true)
         {
+            // 조건에 적합
             if(ls == rs && answer < ls)
             {
                 answer = ls;
             }
+            // 우측이 더 클 경우 좌측 누적합 증가
             else if(ls <= rs && left > 0)
             {
                 ls += cookies[--left];
             }
+            // 좌측이 더 클 경우 우측 누적합 증가
             else if(ls >= rs && right < cookies.size() - 1)
             {
                 rs += cookies[++right];
             }
+            // 범위를 벗어날 경우 다음 i로 이동
             else{
                 break;
             }
