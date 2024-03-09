5e4034d51a8940570a61501cce6c1f0b6f814497
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-03-09 20:04:18 +0900

    [level 2] Title: N-Queen, Time: 128.54 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.cpp
+++ 프로그래머스/2/12952. N－Queen/N－Queen.cpp
@@ -6,27 +6,33 @@ using namespace std;
 const int MAX = 15;
 int col[MAX];
 
+// dfs
+
 bool check(int level)
 {
     for(int i = 0; i < level; i++)
-        if(col[i] == col[level] || abs(col[level] - col[i]) == level - i)// 대각선이거나 같은 라인
-            return false;
+        // 대각선이거나 같은 라인
         //col[i]가 의미하는 것이 X좌표, i가 의미하는것이 Y좌표이므로 차이가 일정하다면 대각선에 있다고 볼 수 있다.
+        if(col[i] == col[level] || abs(col[level] - col[i]) == level - i)
+            return false;
     return true;
 }
 
 void nqueen(int x, int N, int& total)
 {
     if(x == N)
-        total++;
-    else
     {
-        for(int i = 0; i < N; i++)
-        {
-            col[x] = i; // 해당 위치에 퀸을 배치
-            if(check(x)) // 유효하다면 다음행의 퀸 배치, 유효하지않다면 다른 위치로 퀸 배치 변경
-                nqueen(x+1, N, total);
-        }
+        total++;
+        return;
+    }
+    for(int i = 0; i < N; i++)
+    {
+        // 해당 위치에 퀸을 배치
+        col[x] = i; 
+        
+        // 유효하다면 다음행의 퀸 배치, 유효하지않다면 다른 위치로 퀸 배치 변경
+        if(check(x)) 
+            nqueen(x+1, N, total);
     }
 }
 
