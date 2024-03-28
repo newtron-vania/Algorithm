4ebb8374e12f129538a770c667a5a2699fbb9464
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-03-28 21:12:39 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 16 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+const long long DIVIDE_VALUE =  1'000'000'009;
+const int MAX_VALUE = 1'000'000;
+int main()
+{
+    int cnt;
+    cin >> cnt;
+    
+    vector<long long> dp(MAX_VALUE+1, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    
+    for(int i = 4; i <= MAX_VALUE; i++)
+    {
+        dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % DIVIDE_VALUE;
+    }
+    
+    for(int i = 0; i < cnt; i++)
+    {
+        int input;
+        cin >> input;
+        cout << dp[input] << endl;
+    }
+    
+    return 0;
+}
