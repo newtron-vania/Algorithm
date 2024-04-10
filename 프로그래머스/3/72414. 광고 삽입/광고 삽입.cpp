4c87fa3f6b925a23135585f2ba332e5e160a7171
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-04-10 20:21:30 +0900

    [level 3] Title: 광고 삽입, Time: 1.95 ms, Memory: 6.45 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.cpp
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <queue>
 #include <algorithm>
+
 using namespace std;
 
 // 각 초에 대한 재생 횟수를 저장하는 배열
-int cnt[360000];
+const int MAX_VALUE = 360000;
+long long cnt[MAX_VALUE+1];
 
 // 문자열 형식의 시간을 초 단위의 정수로 변환하는 함수
 int strtoint(string s) {
@@ -41,40 +42,41 @@ string inttostr(int i) {
 string solution(string play_time, string adv_time, vector<string> logs) {
     int answer = 0;
     
+    fill_n(cnt, MAX_VALUE+1, 0);
+    
+    int playtime = strtoint(play_time); // 동영상 총 재생 시간
+    int advtime = strtoint(adv_time); // 광고 재생 시간
+    
     // 로그를 분석하여 각 초마다 시청자 수 계산
     for (int i = 0; i < logs.size(); i++) {
         int start = strtoint(logs[i].substr(0, 8));
         int end = strtoint(logs[i].substr(9));
-        for (int j = start; j < end; j++) {
-            cnt[j]++;
-        }
+        
+        cnt[start]++;
+        cnt[end]--;
+    }
+    
+    for(int i = 1; i <= playtime; i++)
+    {
+        cnt[i] += cnt[i-1];
+    }
+    
+    for(int i = 1; i <= playtime; i++)
+    {
+        cnt[i] += cnt[i-1];
     }
 
-    int playtime = strtoint(play_time); // 동영상 총 재생 시간
-    int advtime = strtoint(adv_time); // 광고 재생 시간
+    long long tmp = cnt[advtime - 1]; // 현재 구간의 총 시청자 수
+    long long result = tmp; // 최대 시청자 수
 
-    queue<int> q;
-    long long tmp = 0; // 현재 구간의 총 시청자 수
-    long long result = 0; // 최대 시청자 수
-
-    // 광고 시작 시점이 0초일 때의 총 시청자 수 계산
-    for (int i = 0; i < advtime; i++) {
-        tmp += cnt[i];
-        q.push(cnt[i]);
-    }
-    result = tmp;
-
-    // 투 포인터 알고리즘을 이용하여 최적의 광고 시작 시간 탐색
-    for (int i = advtime; i < playtime; i++) {
-        tmp -= q.front();
-        q.pop();
-        tmp += cnt[i];
-        q.push(cnt[i]);
+    // 슬라이딩 윈도우 알고리즘을 통한 구간합 계산
+    for (int i = 1; i <= playtime - advtime; i++) {
+        tmp = cnt[i + advtime - 1] - cnt[i - 1];
 
         // 구간합이 최대일 때의 시작 시간을 기록
         if (result < tmp) {
             result = tmp;
-            answer = i - advtime + 1;
+            answer = i;
         }
     }
 
