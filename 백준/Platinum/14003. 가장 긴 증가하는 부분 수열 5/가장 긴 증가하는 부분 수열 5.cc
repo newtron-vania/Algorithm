69eb49cfba9892f5a79d894db2baf53af1058cad
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-04-25 21:45:37 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 208 ms, Memory: 21872 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int binary_search(const vector<int>& answer, int val) 
+{
+    int start = 0;
+    int end = answer.size() - 1;
+
+    while (start <= end) 
+    {
+        int mid = (start + end) / 2;
+        if (answer[mid] >= val) 
+        {
+            end = mid - 1;
+        } 
+        else 
+        {
+            start = mid + 1;
+        }
+    }
+    return start;
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> arr[i];
+    }
+
+    vector<int> answer;
+    answer.push_back(-1000000001);
+
+    vector<pair<int, int>> arr_info(n);
+
+    for (int i = 0; i < n; i++) 
+    {
+        if (answer.back() < arr[i]) 
+        {
+            answer.push_back(arr[i]);
+            arr_info[i] = {arr[i], static_cast<int>(answer.size()) - 1};
+        } 
+        else 
+        {
+            int result = binary_search(answer, arr[i]);
+            answer[result] = arr[i];
+            arr_info[i] = {arr[i], result};
+        }
+    }
+
+    cout << answer.size() - 1 << '\n';
+
+    int cnt = answer.size() - 1;
+    vector<int> lis(cnt);
+
+    for (int i = n - 1; i >= 0; i--) 
+    {
+        if (arr_info[i].second == cnt) 
+        {
+            lis[cnt - 1] = arr_info[i].first;
+            cnt--;
+            if (cnt == 0) break;
+        }
+    }
+
+    for (int i = 0; i < lis.size(); i++) 
+    {
+        cout << lis[i] << (i != lis.size() - 1 ? ' ' : '\n');
+    }
+
+    return 0;
+}
