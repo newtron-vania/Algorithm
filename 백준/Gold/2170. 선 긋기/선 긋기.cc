d635f6afca04ceec3a6f3646be2a6a70ef19c7b5
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-05-03 18:36:31 +0900

    [Gold V] Title: 선 긋기, Time: 376 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int n;
+vector<pair<int, int> > arr;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    
+    for(int i = 0 ; i < n ; i++) 
+    {
+        int a,b;
+        cin >> a >> b;
+        arr.push_back({a,b});
+    }
+    
+    sort(arr.begin(), arr.end());
+    
+    int ans = 0;
+    int start = arr[0].first, end = arr[0].second;
+    for(int i = 1; i < n; i++) 
+    {
+        int x1 = arr[i].first, x2 = arr[i].second;
+        if(end > x1) 
+        {
+            end = max(end, x2);
+        } 
+        else 
+        {
+            ans += end-start;
+            start = x1, end = x2;
+        }
+    }
+
+    ans += end-start;
+
+    cout << ans;
+
+    return 0;
+}
