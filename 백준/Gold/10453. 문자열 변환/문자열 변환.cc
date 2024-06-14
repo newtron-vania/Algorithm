9d0a9a4bb40a4342eccd5f547134864e38d00e38
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-06-14 17:32:47 +0900

    [Gold V] Title: 문자열 변환, Time: 48 ms, Memory: 2688 KB -BaekjoonHub

--- 백준/Gold/10453. 문자열 변환/문자열 변환.cc
+++ 백준/Gold/10453. 문자열 변환/문자열 변환.cc
@@ -15,22 +15,16 @@ int main()
         int answer = 0;
         string a, b;
         cin >> a >> b;
-        /* 
-        aababb -> aaabbb 언제나, a, b의 개수가 같고, b의 좌측에 있는 b보다 많으면 좋은 문자열
-        어떻게 인접한 문자를 변경해도 무조건 좋은 문자열이다.
-        */
         
-        vector<int> a_idx;
-        vector<int> b_idx;
+        int a_pos = 0, b_pos = 0;
         for(int i = 0; i < a.length(); i++)
         {
-            if(a[i] == 'a') a_idx.push_back(i);
-            if(b[i] == 'a') b_idx.push_back(i);
-        }
-        
-        for(int i = 0; i < a_idx.size(); i++)
-        {
-            answer += abs(a_idx[i] - b_idx[i]);
+            if(a[i] == 'a')
+            {
+                while(b[b_pos] != 'a') b_pos++;
+                answer += abs(i - b_pos);
+                b_pos++;
+            }
         }
         
         cout << answer << endl;
