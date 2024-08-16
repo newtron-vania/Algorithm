0aedf227f02a349381215bfbbcf64c2f71fbcc18
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-08-16 14:49:31 +0900

    [level 2] Title: 괄호 회전하기, Time: 4.79 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,76 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+
+int solution(string s) {
+    int answer = 0;
+
+    vector<char> stack;
+    vector<int> rotateIdx;
+    int cnt = 0;
+    bool isTrue = false;
+    string checkstr = "]})";
+    for (int i = 0; i < s.size(); i++) {
+        if (checkstr.find(s[i]) != string::npos) {
+            rotateIdx.push_back(i+1);
+        }
+    }
+
+    for (int i = rotateIdx.size()-1; i >0; i--) {
+        rotateIdx[i] -= rotateIdx[i-1];
+    }
+
+
+    int num = 0;
+    while (cnt < s.size()) {
+        isTrue = true;
+        stack.clear();
+        for (char c : s) {
+            if (checkstr.find(c) == string::npos) {
+                stack.push_back(c);
+            }
+            else {
+                if (stack.empty()) {
+                    isTrue = false;
+                    break;
+                }
+                if (stack.back() == '[')
+                    if (c == ']')
+                        stack.pop_back();
+                    else {
+                        isTrue = false;
+                        break;
+                    }
+                else if (stack.back() == '{')
+                    if (c == '}')
+                        stack.pop_back();
+                    else {
+                        isTrue = false;
+                        break;
+                    }
+                else if (stack.back() == '(')
+                    if (c == ')')
+                        stack.pop_back();
+                    else {
+                        isTrue = false;
+                        break;
+                    }
+            }
+        }
+
+        if (stack.empty() && isTrue)
+            answer++;
+        if (num >= rotateIdx.size())
+            break;
+        s = s.substr(rotateIdx[num], s.size() - rotateIdx[num]) + s.substr(0, rotateIdx[num]);
+        cnt += rotateIdx[num];
+        num++;
+        
+    }
+
+    return answer;
+}
