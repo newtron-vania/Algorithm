9a0214c2cebcf90ba5f1d9bae235f6061112e4b9
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2024-08-16 18:19:54 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 41.65 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 
-vector<long long> solution(vector<long long> numbers) {
+vector<long long> solution(vector<long long> numbers)
+{
     vector<long long> answer;
-    for(long long number : numbers){
+    for(long long number : numbers)
+    {
         long long num = 1;
         while((number & num) > 0) num <<= 1;
         answer.push_back(number + num - (num >>1));
