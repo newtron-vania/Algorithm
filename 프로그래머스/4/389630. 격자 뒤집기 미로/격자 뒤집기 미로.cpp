38f7840b3e50204d064acc5f94b5481512e84d02
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-04-29 18:13:13 +0900

    [level 4] Title: 격자 뒤집기 미로, Time: 112.91 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/4/389630. 격자 뒤집기 미로/격자 뒤집기 미로.cpp
+++ 프로그래머스/4/389630. 격자 뒤집기 미로/격자 뒤집기 미로.cpp
@@ -1,183 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 1e9;
-const int NEG_INF = -1e9;
-
-int getOneBitCnt(int mask)
-{
-    int result = 0;
-    while (mask)
-    {
-        result += mask % 2;
-        mask /= 2;
-    }
-    return result;
-}
-
-int solution(vector<vector<int>> visible, vector<vector<int>> hidden, int k)
-{
-    int nRows = visible.size();
-    if (nRows == 0)
-    {
-        return 0;
-    }
-
-    int nCols = visible[0].size();
-    int initSum = 0;
-
-    for (int i = 0; i < nRows; i++)
-    {
-        for (int j = 0; j < nCols; j++)
-        {
-            initSum += visible[i][j];
-        }
-    }
-
-    vector<vector<int>> diff(nRows, vector<int>(nCols, 0));
-    for (int i = 0; i < nRows; i++)
-    {
-        for (int j = 0; j < nCols; j++)
-        {
-            diff[i][j] = hidden[i][j] - visible[i][j];
-        }
-    }
-
-    bool canVisitAllCells = false;
-    if (nRows >= 2 && nCols >= 2)
-    {
-        if (((nRows + nCols) % 2 == 0) && ((nRows * nCols) % 2 == 0))
-        {
-            canVisitAllCells = true;
-        }
-    }
-
-    int answer = NEG_INF;
-
-    for (int rowMask = 0; rowMask < (1 << nRows); rowMask++)
-    {
-        int flipCount = getOneBitCnt(rowMask);
-        int rowCost = flipCount * k;
-        int baseScore = initSum - rowCost;
-
-        vector<int> colFlippedSum(nCols, 0), colNotFlippedSum(nCols, 0);
-
-        for (int col = 0; col < nCols; col++)
-        {
-            int sumFlipped = 0, sumNotFlipped = 0;
-            for (int row = 0; row < nRows; row++)
-            {
-                if (rowMask & (1 << row))
-                {
-                    sumFlipped += diff[row][col];
-                }
-                else
-                {
-                    sumNotFlipped += diff[row][col];
-                }
+auto calc = [](auto a, auto b, int k) {
+    const int n = a.size();
+    const int m = a[0].size();
+    vector dp{ 0, -(1 << 30) };
+    for (int j = 0; j < m; j++) {
+        int sa = 0, mn_a = 1 << 30;
+        int sb = 0, mn_b = 1 << 30;
+        for (int i = 0; i < n; i++) {
+            sa += a[i][j];
+            sb += b[i][j];
+            if (i + j & 1) {
+                mn_a = min(mn_a, a[i][j]);
+                mn_b = min(mn_b, b[i][j]);
             }
-            colFlippedSum[col] = sumFlipped;
-            colNotFlippedSum[col] = sumNotFlipped;
         }
+        vector ndp(2, 0);
+        ndp[0] = dp[0] + max(sa, sb - k);
+        ndp[1] = max(
+            dp[0] + max(sa - mn_a, sb - mn_b - k),
+            dp[1] + max(sa, sb - k)
+        );
+        dp.swap(ndp);
+    }
+    return dp[~n & 1 && ~m & 1];
+};
 
-        if (!canVisitAllCells)
-        {
-            int colTotalScore = 0;
-            for (int col = 0; col < nCols; col++)
-            {
-                colTotalScore += max(colFlippedSum[col], colNotFlippedSum[col] - k);
-            }
-            answer = max(answer, baseScore + colTotalScore);
+int solution(vector<vector<int>> a, vector<vector<int>> b, int k) {
+    const int n = a.size();
+    const int m = a[0].size();
+    int res = -(1 << 30);
+    for (int s = 0; s < 1 << n; s++) {
+        for (int i = 0; i < n; i++) {
+            if (~s >> i & 1) continue;
+            swap(a[i], b[i]);
         }
-        else
-        {
-            vector<int> minOddScoreColNotFlipped(nCols, INF);
-            vector<int> minOddScoreColFlippped(nCols, INF);
-
-            for (int col = 0; col < nCols; col++)
-            {
-                for (int row = 0; row < nRows; row++)
-                {
-                    if (((row + col) & 1) == 1)
-                    {
-                        int oddScoreColNotFlipped;
-                        int oddScoreColFlipped;
-
-                        if (rowMask & (1 << row))
-                        {
-                            oddScoreColNotFlipped = visible[row][col] + diff[row][col];
-                            oddScoreColFlipped = visible[row][col];
-                        }
-                        else
-                        {
-                            oddScoreColNotFlipped = visible[row][col];
-                            oddScoreColFlipped = visible[row][col] + diff[row][col];
-                        }
-
-                        minOddScoreColNotFlipped[col] = min(minOddScoreColNotFlipped[col], oddScoreColNotFlipped);
-                        minOddScoreColFlippped[col] = min(minOddScoreColFlippped[col], oddScoreColFlipped);
-                    }
-                }
-            }
-
-            vector<int> thresholds;
-            for (int col = 0; col < nCols; col++)
-            {
-                thresholds.push_back(minOddScoreColNotFlipped[col]);
-                thresholds.push_back(minOddScoreColFlippped[col]);
-            }
-
-            sort(thresholds.begin(), thresholds.end());
-            thresholds.erase(unique(thresholds.begin(), thresholds.end()), thresholds.end());
-
-            int bestScore = NEG_INF;
-
-            for (int threshold : thresholds)
-            {
-                bool valid = true;
-                int profitSum = 0;
-                int globalOddMin = INF;
-
-                for (int col = 0; col < nCols; col++)
-                {
-                    int bestColProfit = NEG_INF;
-                    int chosenOdd = INF;
-
-                    if (minOddScoreColNotFlipped[col] >= threshold)
-                    {
-                        bestColProfit = colFlippedSum[col];
-                        chosenOdd = minOddScoreColNotFlipped[col];
-                    }
-
-                    if (minOddScoreColFlippped[col] >= threshold)
-                    {
-                        int profitOpt = colNotFlippedSum[col] - k;
-                        if (profitOpt > bestColProfit)
-                        {
-                            bestColProfit = profitOpt;
-                            chosenOdd = minOddScoreColFlippped[col];
-                        }
-                    }
-
-                    if (bestColProfit == NEG_INF)
-                    {
-                        valid = false;
-                        break;
-                    }
-
-                    profitSum += bestColProfit;
-                    globalOddMin = min(globalOddMin, chosenOdd);
-                }
-
-                if (valid)
-                {
-                    bestScore = max(bestScore, baseScore + profitSum - globalOddMin);
-                }
-            }
-
-            answer = max(answer, bestScore);
+        int x = calc(a, b, k);
+        int y = __builtin_popcount(s);
+        res = max(res, x - k * y);
+        for (int i = 0; i < n; i++) {
+            if (~s >> i & 1) continue;
+            swap(a[i], b[i]);
         }
     }
-
-    return answer;
+    return res;
 }
