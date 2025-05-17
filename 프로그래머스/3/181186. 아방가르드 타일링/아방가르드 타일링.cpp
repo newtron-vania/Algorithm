c7e8561892f57aab3a9c27b623a70e10d731317f
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-05-17 17:54:50 +0900

    [level 3] Title: 아방가르드 타일링, Time: 1.45 ms, Memory: 5.23 MB -BaekjoonHub

--- 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.cpp
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.cpp
@@ -16,8 +16,10 @@ long long sum[100002] = { 1, 1, 3, 11, 24, 65, 181, };
 
 const int INF = 1e9 + 7;
 
-int solution(int n) {
-	for (int i = 7; i <= n; i++) {
+int solution(int n)
+{
+	for (int i = 7; i <= n; i++) 
+    {
 		dp[i] = dp[i - 1];
 		dp[i] += dp[i - 2] * 2;
 		dp[i] += dp[i - 3] * 5;
