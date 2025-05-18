4f6816e12961b4e76d78c9bedfa03f319b5c1203
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-05-18 15:38:21 +0900

    [Gold III] Title: 감시, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/15683. 감시/감시.cc
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int DX[4] = {1, 0, -1, 0};
+const int DY[4] = {0, 1, 0, -1};
+
+int n, m;
+int board1[10][10];
+int board2[10][10];
+vector<pair<int, int>> cctv;
+
+bool OOB(int a, int b)
+{
+    return a < 0 || a >= n || b < 0 || b >= m;
+}
+
+void upd(int x, int y, int dir)
+{
+    dir %= 4;
+    while (true)
+    {
+        x += DX[dir];
+        y += DY[dir];
+        if (OOB(x, y) || board2[x][y] == 6)
+            return;
+        if (board2[x][y] != 0)
+            continue;
+        board2[x][y] = 7;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    int mn = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> board1[i][j];
+            if (board1[i][j] != 0 && board1[i][j] != 6)
+                cctv.push_back({i, j});
+            if (board1[i][j] == 0)
+                mn++;
+        }
+    }
+
+    for (int tmp = 0; tmp < (1 << (2 * cctv.size())); tmp++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                board2[i][j] = board1[i][j];
+            }
+        }
+
+        int brute = tmp;
+        for (int i = 0; i < cctv.size(); i++)
+        {
+            int dir = brute % 4;
+            brute /= 4;
+            int x = cctv[i].first;
+            int y = cctv[i].second;
+
+            if (board1[x][y] == 1)
+            {
+                upd(x, y, dir);
+            }
+            else if (board1[x][y] == 2)
+            {
+                upd(x, y, dir);
+                upd(x, y, dir + 2);
+            }
+            else if (board1[x][y] == 3)
+            {
+                upd(x, y, dir);
+                upd(x, y, dir + 1);
+            }
+            else if (board1[x][y] == 4)
+            {
+                upd(x, y, dir);
+                upd(x, y, dir + 1);
+                upd(x, y, dir + 2);
+            }
+            else
+            {
+                upd(x, y, dir);
+                upd(x, y, dir + 1);
+                upd(x, y, dir + 2);
+                upd(x, y, dir + 3);
+            }
+        }
+
+        int val = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                val += (board2[i][j] == 0);
+            }
+        }
+
+        mn = min(mn, val);
+    }
+
+    cout << mn;
+}
