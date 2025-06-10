13b76e86b5484f4fa58d1a81ac5370eb04f08d76
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-06-10 14:41:15 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.47 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -9,7 +9,8 @@ using namespace std;
 
 vector<string> Split(string str, char delimeter);
 
-vector<int> solution(vector<int> fees, vector<string> records) {
+vector<int> solution(vector<int> fees, vector<string> records) 
+{
     vector<int> answer;
     //기본 시간, 시간 요금, 단위 시간, 단위 요금
     int basicTime = fees[0];
@@ -19,14 +20,16 @@ vector<int> solution(vector<int> fees, vector<string> records) {
     
     map<string, int> recordDict;
     map<string, int> recordAnswer;
-    for(string record : records){
+    for(string record : records)
+    {
         vector<string> str = Split(record, ' ');
         int time = stoi(str[0].substr(0,2)) * 60 + stoi(str[0].substr(3,2));
         //입차 시간 등록
         if(str[2] == "IN"){
             recordDict[str[1]] = time;
         }
-        else{
+        else
+        {
             //해당 차량 누적 주차 시간 저장
             recordAnswer[str[1]] += time - recordDict[str[1]];
             //출차 시 입차 시간 초기화
@@ -34,10 +37,12 @@ vector<int> solution(vector<int> fees, vector<string> records) {
         }
     }
     //map이라서 key 순으로 자동 정렬
-    for(auto& itr : recordDict){
+    for(auto& itr : recordDict)
+    {
         //입차 시간이 초기화된 상태가 아니라면 == 입차 후 출차한 차량이 아님
         //23:59에 출차된 것으로 간주
-        if(itr.second >= 0){
+        if(itr.second >= 0)
+        {
             recordAnswer[itr.first] += 24*60 - 1 - itr.second;
             itr.second = 0;
         }
@@ -50,11 +55,13 @@ vector<int> solution(vector<int> fees, vector<string> records) {
     return answer;
 }
 //Split 함수
-vector<string> Split(string str, char delimeter){
+vector<string> Split(string str, char delimeter)
+{
     string tmp;
     vector<string> result;
     istringstream ss(str);
-    while(getline(ss, tmp, delimeter)){
+    while(getline(ss, tmp, delimeter))
+    {
         result.push_back(tmp);
     }
     return result;
