4024444803de4ef721db8a296a1a25bf0c2f7924
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-06-23 21:54:30 +0900

    [level 2] Title: 전화번호 목록, Time: 214.96 ms, Memory: 260 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -2,17 +2,66 @@
 
 using namespace std;
 
+class TrieNode
+{
+private:
+    TrieNode* children[10] = { nullptr };
+    int count = 0;
+public:
+    TrieNode() {}
+    
+    void insert(const string& numbers)
+    {
+        TrieNode* node = this;
+        for (char c : numbers)
+        {
+            node->count++;
+            int idx = c - '0';
+            if (!node->children[idx])
+            {
+                node->children[idx] = new TrieNode();
+            }
+            node = node->children[idx];
+        }
+    }
+
+    bool search(const string& pattern)
+    {
+        TrieNode* node = this;
+        for (char c : pattern)
+        {
+            int idx = c - '0';
+            if (!node->children[idx]) return false;
+            node = node->children[idx];
+        }
+        
+        for(const auto& child : node->children)
+        {
+            if(child != nullptr) return false;
+        }
+        
+        return true;
+    }
+};
+
 bool solution(vector<string> phone_book) 
 {
-    sort(phone_book.begin(), phone_book.end());
     bool answer = true;
-    for(int i = 0; i < phone_book.size()-1; i++)
+    
+    TrieNode* TrieTree = new TrieNode();
+    for(const auto& number : phone_book)
     {
-        if(!phone_book[i+1].substr(0, phone_book[i].size()).compare(phone_book[i]))
+        TrieTree->insert(number);
+    }
+    
+    for(const auto& number : phone_book)
+    {
+        if(!TrieTree->search(number))
         {
             answer = false;
             break;
         }
     }
+    
     return answer;
 }
