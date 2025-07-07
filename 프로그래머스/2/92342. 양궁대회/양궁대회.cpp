08d0752c6ea0ba3fc078cd4d37d530f711d8950c
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-07-07 21:56:22 +0900

    [level 2] Title: 양궁대회, Time: 0.20 ms, Memory: 3.75 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
@@ -1,66 +1,63 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
 
 using namespace std;
 
-vector<int> solution(int n, vector<int> info) 
-{
-    vector<int> answer(11, -1);
-    
-    int max_diff = -1;
-    
-    for(int i = 0; i < (1 << 11); i++)
-    {
-        int lion_score = 0;
-        int peach_score = 0;
-        int arrow_count = n;
-        vector<int> lion_info(11, 0);
-        for(int j = 10; j >= 0; j--)
-        {
-            if(i & (1 << j))
-            {
-                if(arrow_count > info[10 - j])
-                {
-                    lion_score += j;
-                    arrow_count -= info[10 - j] + 1;
-                    lion_info[10 - j] = info[10 - j] + 1;
-                }
-                else if(info[10 - j] > 0)
-                {
-                    peach_score += j;
-                }
-            }
-            else if(info[10 - j] > 0)
-            {
-                peach_score += j;
-            }
-            
-        }
-        lion_info[10] += arrow_count;
+bool is_better(const vector<int>& a, const vector<int>& b) {
+    for (int i = 10; i >= 0; --i) {
+        if (a[i] > b[i]) return true;
+        else if (a[i] < b[i]) return false;
+    }
+    return false; // 같음
+}
+
+vector<int> solution(int n, vector<int> info) {
+    int maxScoreDiff = 0;
+    int maxBit = 1;
+    vector<int> maxArray(11);
+    for(int bit = 1; bit != (1 << 11); bit++){
+        int count = 0;
+        int score = 0;
+        int targetScore = 0;
         
-        int diff = lion_score - peach_score;
-        if(diff <= 0) continue;
-        if(diff > max_diff)
-        {
-            max_diff = diff;
-            answer = lion_info;
+        vector<int> array(11);
+        
+        for(int i = 0; i < 10; i++){
+            if(bit&(1<<i)){
+                array[i] += info[i] + 1;
+                count += info[i] + 1;
+                score += 10 - i;
+            }
+            else{
+                if(info[i] != 0)
+                    targetScore += 10 - i;
+            }
         }
-        else if (diff == max_diff)
-        {
-            // 낮은 점수 많이 쏜 걸 우선시
-            for (int i = 10; i >= 0; i--) 
-            {
-                if (lion_info[i] > answer[i]) 
-                {
-                    answer = lion_info;
-                    break;
-                } 
-                else if (lion_info[i] < answer[i]) 
-                {
-                    break;
+        if(count <= n){
+            if(count < n){
+                array[10] += n - count;
+            }
+            if(targetScore < score){
+                int scoreDiff = score - targetScore;
+                if(maxScoreDiff < scoreDiff){
+                    maxScoreDiff = scoreDiff;
+                    maxBit = bit;
+                    maxArray = array;
+                }
+                else if(maxScoreDiff == scoreDiff){
+                    if (is_better(array, maxArray)){
+                        maxBit = bit;
+                        maxArray = array;
+                    }
                 }
             }
         }
     }
-    
-    return max_diff > 0 ? answer : vector<int>{ -1 };
+    if(maxScoreDiff <= 0){
+        vector<int> answer = {-1};
+        return answer;
+    }
+    return maxArray;
 }
