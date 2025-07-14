aae58e3eea082b670b673c0ff3e5c228667c99ae
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-07-14 18:25:28 +0900

    [level 3] Title: 징검다리 건너기, Time: 3.64 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool binary_check(vector<int>& stones, int k, int mid)
+{
+    int cnt = 0;
+    for(int i = 0; i < stones.size(); i++)
+    {
+        if(stones[i] <= mid) cnt += 1;
+        else cnt = 0;
+        if(cnt >= k) return false;
+    }
+    return true;
+}
+
+int solution(vector<int> stones, int k) 
+{
+    int answer = 0;
+    int left = 1;
+    int right = *max_element(stones.begin(), stones.end());
+    
+    while(left <= right)
+    {
+        int mid = (left + right) / 2;
+        if(!binary_check(stones, k, mid))
+        {
+            right = mid - 1;
+            answer = mid;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+    return answer;
+}
