71bbf9f53b10b3432ca22aeedeab59cb4614e19c
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-07-14 22:21:35 +0900

    [level 2] Title: 할인 행사, Time: 10.22 ms, Memory: 8.79 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -1,47 +1,35 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
+bool CanSale(vector<int> number) {
+    for (vector<int>::iterator i = number.begin(); i != number.end(); i++) {
+        if (*i > 0) return false;
+    }
+    return true;
+}
+
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
-    unordered_map<string, int> want_counts;
-    
-    for(string w : want)
-    {
-        want_counts.insert(pair(w, 0));
+    for (int i = 0; i < 10; i++) {
+        // 해당 문자열 찾아서 갯수 줄이기
+        auto it = find(want.begin(), want.end(), discount[i]);
+        if (it != want.end()) number[it - want.begin()] -= 1;
     }
-    
-    for(int i = 0; i < discount.size(); i++)
-    {
-        
-        bool isOver = true;
-        if(want_counts.find(discount[i]) != want_counts.end())
-        {
-            want_counts[discount[i]] += 1;
-        }
-        
-        if(i >= 10)
-        {
-            if(want_counts.find(discount[i-10]) != want_counts.end())
-            {
-                want_counts[discount[i-10]] -= 1;
-            }
-        }
-        
-        for(int j = 0; j < want_counts.size(); j++)
-        {
-            if(want_counts[want[j]] < number[j])
-            {
-                isOver = false;
-                break;
-            }
-        }
-    
-        if(isOver)
-        {
-            answer += 1;
-        }
+    int discountSize = 0;
+    if (CanSale(number)) discountSize += 1;
+    answer = discountSize;
+    for (int i = 10; i < discount.size(); i++) {
+        auto it = find(want.begin(), want.end(), discount[i - 10]);
+        if (it != want.end()) number[it - want.begin()] += 1;
+        it = find(want.begin(), want.end(), discount[i]);
+        if (it != want.end()) number[it - want.begin()] -= 1;
+
+        if (CanSale(number)) discountSize += 1;
     }
-    
+    if (answer < discountSize) answer = discountSize;
     return answer;
 }
