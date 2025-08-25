2dba318fa33af359e64ccf438393e63ea8e19219
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-08-25 20:26:49 +0900

    [Gold IV] Title: 뒤집기 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1464. 뒤집기 3/뒤집기 3.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    string s = str.substr(0,1);
+    for(int i = 1; i< str.size(); i++)
+    {
+        if(s[i-1] < str[i])
+        {
+            s = str[i] + s;
+        }else
+        {
+            s = s + str[i];
+        }
+    }
+    
+    reverse(s.begin(), s.end());
+    cout << s;
+    return 0;
+}
