0b1583d840ec84aaaeb60bb1e4e80d0de1ddbdd6
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-08-26 11:12:38 +0900

    [level 2] Title: 비밀 코드 해독, Time: 1.60 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
@@ -1,43 +1,28 @@
 #include <string>
 #include <vector>
- 
+#include <algorithm>
+#include <iostream>
+
 using namespace std;
- 
-int answer;
- 
-void check(int pwd, vector<vector<int>> q, vector<int> ans)
-{
-    for(int i = 0; i < q.size(); i++)
-    {
-        int compare = 0, cnt = 0;
-        for(int bit : q[i]) compare += (1 << bit-1);
-        cnt = __builtin_popcount(pwd & compare);
-        if(cnt != ans[i]) return;
+
+int solution(int n, vector<vector<int>> q, vector<int> ans) {
+    int m = q.size();
+    int answer = 0;
+    vector<int> comb(n);
+    for (int i = 0; i < 5; ++i) {
+        comb[n - i - 1] = 1;
     }
-    answer++;
-}
- 
-int solution(int n, vector<vector<int>> q, vector<int> ans) 
-{
-    int pwd = 0;
-    
-    for(int i=1; i<=n-4; i++)
-    {
-        for(int j=i+1; j<=n-3; j++)
-        {
-            for(int k=j+1; k<=n-2; k++)
-            {
-                for(int l=k+1; l<=n-1; l++)
-                {
-                    for(int m=l+1; m<=n; m++)
-                    {
-                        pwd = (1 << (i-1)) | (1 << (j-1)) | (1 << (k-1)) | (1 << (l-1)) | (1 << (m-1));
-                        check(pwd, q, ans);
-                    }
-                }
+
+    do {
+        bool f = true;
+        for (int i = 0; i < m; ++i) {
+            if (ans[i] != comb[q[i][0] - 1] + comb[q[i][1] - 1] + comb[q[i][2] - 1] + comb[q[i][3] - 1] + comb[q[i][4] - 1]) {
+                f = false;
+                break;
             }
         }
-    }
- 
+
+        if (f) ++answer;
+    } while(next_permutation(comb.begin(), comb.end()));
     return answer;
 }
