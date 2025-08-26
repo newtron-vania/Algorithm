f41092e10b1f8d7e1abec25dd92f8ab305e02c70
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-08-26 11:20:28 +0900

    [level 2] Title: 비밀 코드 해독, Time: 1.59 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
@@ -5,23 +5,27 @@
 
 using namespace std;
 
-int solution(int n, vector<vector<int>> q, vector<int> ans) {
+int solution(int n, vector<vector<int>> q, vector<int> ans)
+{
     int m = q.size();
     int answer = 0;
     vector<int> comb(n);
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i)
+    {
         comb[n - i - 1] = 1;
     }
 
-    do {
+    do 
+    {
         bool f = true;
-        for (int i = 0; i < m; ++i) {
-            if (ans[i] != comb[q[i][0] - 1] + comb[q[i][1] - 1] + comb[q[i][2] - 1] + comb[q[i][3] - 1] + comb[q[i][4] - 1]) {
+        for (int i = 0; i < m; ++i) 
+        {
+            if (ans[i] != comb[q[i][0] - 1] + comb[q[i][1] - 1] + comb[q[i][2] - 1] + comb[q[i][3] - 1] + comb[q[i][4] - 1]) 
+            {
                 f = false;
                 break;
             }
         }
-
         if (f) ++answer;
     } while(next_permutation(comb.begin(), comb.end()));
     return answer;
