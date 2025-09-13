6235c4f4a2a4847889a3495c5c65b0b6ada526a7
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-09-13 17:24:05 +0900

    [Gold IV] Title: AB, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12970. AB/AB.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve(int N, int K)
+{
+    
+}
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N, K;
+    
+    cin >> N >> K;
+    
+    if(K > (N / 2) * (N / 2 + N % 2))
+    {
+        cout << -1;
+    }
+    else if(K == 0)
+    {
+        for(int i = 0; i < N - 1; i++)
+            cout << 'B';
+        cout << 'A';
+    }
+    else 
+    {
+        for(int i = 0, j = 1, sum = 0; i < N; i++)
+        {
+            if(N - j - i > 0 && sum + N - j - i <= K)
+            {    
+                cout << 'A';
+                sum += N - j++ - i;
+            }
+            else
+                cout << 'B';
+        }
+    }
+}
