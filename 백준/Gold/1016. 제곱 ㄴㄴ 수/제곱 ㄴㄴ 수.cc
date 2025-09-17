1ff430d884ca8217a285e789262055d9d77a3852
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-09-18 06:41:14 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int doubles[1000001];
+
+int main()
+{
+    long long min_v, max_v;
+    
+    scanf("%lld %lld", &min_v, &max_v);
+    
+    int cnt = max_v - min_v + 1;
+    
+    for(long long k = 2; k * k <= max_v; k++)
+    {
+        long long kk = k * k;
+        long long ret = min_v / kk;
+		if (min_v % kk != 0)
+        {
+            ret++;
+        }
+        
+		while (ret * kk <= max_v)
+		{	
+			if (doubles[ret * kk - min_v] == false)
+            {
+				doubles[ret * kk - min_v] = true;
+				cnt--;
+			}
+			ret++;
+		}
+    }
+    
+    printf("%d", cnt);
+}
