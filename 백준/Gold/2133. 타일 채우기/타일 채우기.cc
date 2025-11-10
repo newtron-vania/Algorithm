7e3fd367950b8e2100f4f38abc0459e593e4c9b9
Kim Kyeongsoo <118050445+newtron-vania@users.noreply.github.com>
2025-11-10 17:17:56 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.cc
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -1,38 +1,25 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-
-vector<int> dp(31, 0);
-
-int DFS(int n)
-{
-    if (n % 2 == 1)
-    {
-        return 0;
-    }
-    if (n <= 2)
-    {
-        return dp[n];
-    }
-    if (dp[n] != 0)
-    {
-        return dp[n];
-    }
-
-    dp[n] = 3 * DFS(n - 2);
-    for (int i = n - 4; i >= 0; i -= 2)
-    {
-        dp[n] += 2 * DFS(i);
-    }
-
-    return dp[n];
-}
 
 int main()
 {
     int n;
     cin >> n;
+
+    vector<int> dp(31, 0);
+
     dp[0] = 1;
     dp[2] = 3;
-    cout << DFS(n);
-    return 0;
+
+    for (int i = 4; i <= n; i += 2)
+    {
+        dp[i] = 3 * dp[i - 2];
+        for (int j = i - 4; j >= 0; j -= 2)
+        {
+            dp[i] += 2 * dp[j];
+        }
+    }
+
+    cout << dp[n];
 }
